6268e9d0496c39c534c35c6ee6f08b1c2220b068
Adam Wierzbicki <awierzbicki@golem.network>
2019-10-18 17:08:48 +0200

    Environment coordinator

--- golem/envs/auto_setup.py
+++ golem/envs/auto_setup.py
@@ -3,8 +3,7 @@ from typing import (
     Callable,
     Dict,
     Optional,
-    Tuple,
-    Type
+    Tuple
 )
 
 from twisted.internet.defer import (
@@ -20,7 +19,6 @@ from golem.envs import (
     EnvEventListener,
     EnvEventType,
     Environment,
-    EnvMetadata,
     EnvStatus,
     EnvSupportStatus,
     Prerequisites,
@@ -91,100 +89,125 @@ class RuntimeSetupWrapper(Runtime):
         self._runtime.listen(event_type, listener)
 
 
-def auto_setup(env: Environment) -> Environment:
+class EnvCoordinator:
+    """ Ensures that only one environment is used at a time. Lazily cleans up
+        unused environments. """
 
-    class EnvSetupWrapper(Environment):
+    def __init__(self) -> None:
+        self._lock = DeferredLock()
+        self._active_env: Optional[Environment] = None
 
-        def __init__(self) -> None:
-            self._num_users = 0
-            self._lock = DeferredLock()
+    @inlineCallbacks
+    def start_usage(self, env: Environment) -> Deferred:
+        yield self._lock.acquire()
+        if self._active_env is env:
+            return
+        if self._active_env is not None:
+            yield self._active_env.clean_up()
+        self._active_env = env
+        yield env.prepare()
 
-        @inlineCallbacks
-        def _start_usage(self) -> Deferred:
-            yield self._lock.acquire()
-            try:
-                if self._num_users == 0:
-                    yield env.prepare()
-                self._num_users += 1
-            finally:
-                self._lock.release()
+    @inlineCallbacks
+    def end_usage(self, env: Environment) -> Deferred:
+        if self._active_env is not env:
+            raise ValueError('end_usage called for wrong environment')
+        yield self._lock.release()
 
-        @inlineCallbacks
-        def _end_usage(self) -> Deferred:
-            yield self._lock.acquire()
-            try:
-                self._num_users -= 1
-                if self._num_users == 0:
-                    yield env.clean_up()
-            finally:
-                self._lock.release()
+    def auto_setup(self, env: Environment) -> Environment:
+        coordinator = self
 
-        @classmethod
-        def supported(cls) -> EnvSupportStatus:
-            return env.supported()
+        class EnvSetupWrapper(Environment):
 
-        def status(self) -> EnvStatus:
-            return env.status()
+            def __init__(self) -> None:
+                self._num_users = 0
+                self._lock = DeferredLock()
 
-        def prepare(self) -> Deferred:
-            raise AttributeError('prepare and clean_up not supported')
+            @inlineCallbacks
+            def _start_usage(self) -> Deferred:
+                yield self._lock.acquire()
+                try:
+                    if self._num_users == 0:
+                        yield coordinator.start_usage(env)
+                    self._num_users += 1
+                finally:
+                    self._lock.release()
 
-        def clean_up(self) -> Deferred:
-            raise AttributeError('prepare and clean_up not supported')
+            @inlineCallbacks
+            def _end_usage(self) -> Deferred:
+                yield self._lock.acquire()
+                try:
+                    self._num_users -= 1
+                    if self._num_users == 0:
+                        yield coordinator.end_usage(env)
+                finally:
+                    self._lock.release()
 
-        @inlineCallbacks
-        def run_benchmark(self) -> Deferred:
-            yield self._start_usage()
-            try:
-                return (yield env.run_benchmark())
-            finally:
-                yield self._end_usage()
+            @classmethod
+            def supported(cls) -> EnvSupportStatus:
+                return env.supported()
 
-        @classmethod
-        def parse_prerequisites(
-                cls,
-                prerequisites_dict: Dict[str, Any]
-        ) -> Prerequisites:
-            return env.parse_prerequisites(prerequisites_dict)
+            def status(self) -> EnvStatus:
+                return env.status()
 
-        @inlineCallbacks
-        def install_prerequisites(
-                self,
-                prerequisites: Prerequisites
-        ) -> Deferred:
-            yield self._start_usage()
-            try:
-                return (yield env.install_prerequisites(prerequisites))
-            finally:
-                yield self._end_usage()
+            def prepare(self) -> Deferred:
+                raise AttributeError('prepare and clean_up not supported')
 
-        @classmethod
-        def parse_config(cls, config_dict: Dict[str, Any]) -> EnvConfig:
-            return env.parse_config(config_dict)
+            def clean_up(self) -> Deferred:
+                raise AttributeError('prepare and clean_up not supported')
 
-        def config(self) -> EnvConfig:
-            return env.config()
+            @inlineCallbacks
+            def run_benchmark(self) -> Deferred:
+                yield self._start_usage()
+                try:
+                    return (yield env.run_benchmark())
+                finally:
+                    yield self._end_usage()
 
-        def update_config(self, config: EnvConfig) -> None:
-            env.update_config(config)
+            @classmethod
+            def parse_prerequisites(
+                    cls,
+                    prerequisites_dict: Dict[str, Any]
+            ) -> Prerequisites:
+                return env.parse_prerequisites(prerequisites_dict)
 
-        def listen(
-                self,
-                event_type: EnvEventType,
-                listener: EnvEventListener
-        ) -> None:
-            env.listen(event_type, listener)
+            @inlineCallbacks
+            def install_prerequisites(
+                    self,
+                    prerequisites: Prerequisites
+            ) -> Deferred:
+                yield self._start_usage()
+                try:
+                    return (yield env.install_prerequisites(prerequisites))
+                finally:
+                    yield self._end_usage()
 
-        def runtime(
-                self,
-                payload: RuntimePayload,
-                config: Optional[EnvConfig] = None
-        ) -> Runtime:
-            runtime = env.runtime(payload, config)
-            return RuntimeSetupWrapper(
-                runtime=runtime,
-                start_usage=self._start_usage,
-                end_usage=self._end_usage
-            )
+            @classmethod
+            def parse_config(cls, config_dict: Dict[str, Any]) -> EnvConfig:
+                return env.parse_config(config_dict)
 
-    return EnvSetupWrapper()
+            def config(self) -> EnvConfig:
+                return env.config()
+
+            def update_config(self, config: EnvConfig) -> None:
+                env.update_config(config)
+
+            def listen(
+                    self,
+                    event_type: EnvEventType,
+                    listener: EnvEventListener
+            ) -> None:
+                env.listen(event_type, listener)
+
+            def runtime(
+                    self,
+                    payload: RuntimePayload,
+                    config: Optional[EnvConfig] = None
+            ) -> Runtime:
+                runtime = env.runtime(payload, config)
+                return RuntimeSetupWrapper(
+                    runtime=runtime,
+                    start_usage=self._start_usage,
+                    end_usage=self._end_usage
+                )
+
+        return EnvSetupWrapper()
